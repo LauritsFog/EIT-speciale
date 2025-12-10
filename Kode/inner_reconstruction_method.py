d1a542ac93f4606e92ad1e4dec96716a48c9d807
Magnushhhh <101399444+Magnushhhh@users.noreply.github.com>
2025-12-10 13:36:02 +0100

    symmetri update og start på warping

--- Kode/inner_reconstruction_method.py
+++ Kode/inner_reconstruction_method.py
@@ -240,6 +240,7 @@ def find_inclusion_elements(
                     Dfrechet[i, j] = -const * grad_dot * area
 
             check = Dfrechet - ntd_AD + ntd_A0
+            check = (check + check.T) / 2  # ensure symmetry
             min_eig = np.min(np.real(np.linalg.eigvals(check)))
 
             min_eig_values.append(min_eig)
@@ -273,6 +274,7 @@ def find_inclusion_elements(
                         Dfrechet[i, j] -= const * grad_dot * area
 
             check = Dfrechet - ntd_AD + ntd_A0
+            check = (check + check.T) / 2  # ensure symmetry
             min_eig = np.min(np.real(np.linalg.eigvals(check)))
             is_inclusion = min_eig + tol > 0
 
